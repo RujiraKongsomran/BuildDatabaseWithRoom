ee2101603b41e008abeab8ee50dffa9f88f6f9ce
Rujira Kongsomran <biwberry@outlook.com>
2020-05-31 16:25:05 +0700

    Update and Delete

--- app/src/main/java/com/rujirakongsomran/builddatabasewithroom/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/builddatabasewithroom/MainActivity.java
@@ -1,13 +1,19 @@
 package com.rujirakongsomran.builddatabasewithroom;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,10 +27,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
@@ -143,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_clear:
                 deleteAllUsers();
                 break;
@@ -179,5 +183,131 @@ public class MainActivity extends AppCompatActivity {
                                }
                            }
                 );
+        compositeDisposable.add(compositeDisposable);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        menu.setHeaderTitle("Select action:");
+
+        menu.add(Menu.NONE, 0, Menu.NONE, "UPDATE");
+        menu.add(Menu.NONE, 1, Menu.NONE, "DELETE");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final User user = userList.get(info.position);
+        switch (item.getItemId()) {
+            case 0: // update
+            {
+                final EditText etName = new EditText(MainActivity.this);
+                etName.setText(user.getName());
+                etName.setHint("Enter your name");
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Edit")
+                        .setMessage("Edit user name")
+                        .setView(etName)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                if (TextUtils.isEmpty(etName.getText().toString())) {
+                                    return;
+                                } else {
+                                    user.setName(etName.getText().toString());
+                                    updateUser(user);
+                                }
+                            }
+                        }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create().show();
+
+            }
+            break;
+            case 1: // Delete
+            {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage("Do you want to delete " + user.toString())
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                deleteUser(user);
+                            }
+                        }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create().show();
+
+            }
+            break;
+        }
+        return true;
+    }
+
+    private void deleteUser(final User user) {
+        Disposable disposable = io.reactivex.Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
+                userRepository.deleteUser(user);
+                emitter.onComplete();
+            }
+        })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Consumer() {
+                               @Override
+                               public void accept(Object o) throws Exception {
+
+                               }
+                           }, new Consumer<Throwable>() {
+                               @Override
+                               public void accept(Throwable throwable) throws Exception {
+                                   Toast.makeText(MainActivity.this, "" + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                               }
+                           }, new Action() {
+                               @Override
+                               public void run() throws Exception {
+                                   loadData(); // Refresh data
+                               }
+                           }
+                );
+        compositeDisposable.add(disposable);
+    }
+
+    private void updateUser(final User user) {
+        Disposable disposable = io.reactivex.Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
+                userRepository.updateUser(user);
+                emitter.onComplete();
+            }
+        })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Consumer() {
+                               @Override
+                               public void accept(Object o) throws Exception {
+
+                               }
+                           }, new Consumer<Throwable>() {
+                               @Override
+                               public void accept(Throwable throwable) throws Exception {
+                                   Toast.makeText(MainActivity.this, "" + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                               }
+                           }, new Action() {
+                               @Override
+                               public void run() throws Exception {
+                                   loadData(); // Refresh data
+                               }
+                           }
+                );
+        compositeDisposable.add(disposable);
     }
 }
