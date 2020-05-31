60e8d90c1d56792a5611e9d9ea1fb2cbe1c98ff9
Rujira Kongsomran <biwberry@outlook.com>
2020-05-31 13:16:44 +0700

    Create Interface User DAO

---
+++ app/src/main/java/com/rujirakongsomran/builddatabasewithroom/Local/UserDAO.java
@@ -0,0 +1,32 @@
+package com.rujirakongsomran.builddatabasewithroom.Local;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.rujirakongsomran.builddatabasewithroom.Model.User;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+
+public interface UserDAO {
+    @Query("SELECT * FROM users WHERE id=:userId")
+    Flowable<User> getUserById(int userId);
+
+    @Query("SELECT * FROM users")
+    Flowable<List<User>> getAllUsers();
+
+    @Insert
+    void insertUser(User... users);
+
+    @Update
+    void updateUser(User... users);
+
+    @Delete
+    void deleteUser(User user);
+
+    @Query("DELETE FROM users")
+    void deleteAllUsers();
+}
