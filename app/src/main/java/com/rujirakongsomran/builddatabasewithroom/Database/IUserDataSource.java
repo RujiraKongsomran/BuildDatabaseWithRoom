834f9beb72411e3549b048f1083be8bae5e21827
Rujira Kongsomran <biwberry@outlook.com>
2020-05-31 13:56:04 +0700

    Create Interface UserDataSource

---
+++ app/src/main/java/com/rujirakongsomran/builddatabasewithroom/Database/IUserDataSource.java
@@ -0,0 +1,23 @@
+package com.rujirakongsomran.builddatabasewithroom.Database;
+
+
+import com.rujirakongsomran.builddatabasewithroom.Model.User;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+
+public interface IUserDataSource {
+
+    Flowable<User> getUserById(int userId);
+
+    Flowable<List<User>> getAllUsers();
+
+    void insertUser(User... users);
+
+    void updateUser(User... users);
+
+    void deleteUser(User user);
+
+    void deleteAllUsers();
+}
